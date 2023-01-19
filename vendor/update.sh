34e560e1f8d27b947fda8785a2e3c19a4505ee44
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2023-01-19 15:16:27 +0000

    Vendor update script

---
+++ vendor/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export VENDOR="$(pwd)/vendor"
+echo "Vendor path: $VENDOR"
+
+echo "[+] Delete all folders in vendor"
+rm -rf "$VENDOR/*/"
+
+echo "[+] Install all dependencies"
+pipenv run pip freeze > "$VENDOR/requirements.txt"
+pip install requests --target=$VENDOR
+
+echo "[+] Clean up vendor folder"
+rm -rf $VENDOR/*dist-info && \
+    rm -rf $VENDOR/requirements.txt
+
+echo "Completed vendor update"
