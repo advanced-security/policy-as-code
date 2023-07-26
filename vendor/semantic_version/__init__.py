c2547e39efc7892c0b890bbba18215b5e8100bd8
GeekMasher <geekmasher@gmail.com>
2023-07-26 13:39:15 +0100

    feat: patch semantic_version

--- vendor/semantic_version/__init__.py
+++ vendor/semantic_version/__init__.py
@@ -5,14 +5,5 @@
 
 from .base import compare, match, validate, SimpleSpec, NpmSpec, Spec, SpecItem, Version
 
-
 __author__ = "Raphaël Barrois <raphael.barrois+semver@polytechnique.org>"
-try:
-    # Python 3.8+
-    from importlib.metadata import version
-
-    __version__ = version("semantic_version")
-except ImportError:
-    import pkg_resources
-
-    __version__ = pkg_resources.get_distribution("semantic_version").version
+__version__ = "2.10.0"
