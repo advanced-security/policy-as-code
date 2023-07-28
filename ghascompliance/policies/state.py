74ca2704057dc27e4791e7dd52937ba1b7e9e20c
GeekMasher <geekmasher@gmail.com>
2023-07-28 14:34:42 +0100

    feat: update policies state docs

--- ghascompliance/policies/state.py
+++ ghascompliance/policies/state.py
@@ -1,9 +1,13 @@
+"""PolicyState."""
 from typing import Any
 from ghascompliance.octokit.octokit import Octokit
 
 
 class PolicyState:
+    """PolicyState."""
+
     def __init__(self, name: str) -> None:
+        """Initialise PolicyState."""
         self.name = name
 
         self.criticals = []
@@ -12,18 +16,21 @@ class PolicyState:
         self.ignored = []
 
     def critical(self, data: Any, trigger_name: str = "na"):
+        """Add critical error to the state."""
         if isinstance(data, str):
             self.criticals.append({"msg": data, "trigger": trigger_name})
         else:
             Octokit.warning(f"Unknown critical type :: {type(data)}")
 
     def error(self, data: Any, trigger_name: str = "na"):
+        """Add error to the state."""
         if isinstance(data, str):
             self.errors.append({"msg": data, "trigger": trigger_name})
         else:
             Octokit.warning(f"Unknown error type :: {type(data)}")
 
     def warning(self, data: Any, trigger_name: str = "na"):
+        """Add warning to the state."""
         if isinstance(data, str):
             self.warnings.append({"msg": data, "trigger": trigger_name})
         else:
