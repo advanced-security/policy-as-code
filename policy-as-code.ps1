b021050feb37da5b8065fa70a67a835a8f099054
GeekMasher <geekmasher@gmail.com>
2024-01-31 13:30:21 +0000

    fix(cli): Fix Powershell script PYTHONPATH issue

--- policy-as-code.ps1
+++ policy-as-code.ps1
@@ -3,7 +3,7 @@
 $pw = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 if (Get-Command python -ErrorAction SilentlyContinue) {
-    $env:PYTHONPATH = $pw/vendor
+    $env:PYTHONPATH = $pw + "\vendor"
     python -m ghascompliance $args
 } else {
     Write-Host "Python is not installed. Please install Python (+3.9) and try again."
