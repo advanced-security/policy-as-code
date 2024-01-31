a91b56ec7f524906c51a7c2e3142a4d4a5c12300
GeekMasher <geekmasher@gmail.com>
2024-01-31 12:22:38 +0000

    feat(cli): Add policy as code bash and powershell scripts

---
+++ policy-as-code.ps1
@@ -0,0 +1,12 @@
+# Policy as Code Powershell script.
+
+$pw = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+if (Get-Command python -ErrorAction SilentlyContinue) {
+    $env:PYTHONPATH = $pw/vendor
+    python -m ghascompliance $args
+} else {
+    Write-Host "Python is not installed. Please install Python (+3.9) and try again."
+    Write-Host "Make sure that Python is added to the PATH environment variable."
+}
+
